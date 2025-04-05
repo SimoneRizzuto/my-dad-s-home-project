d65c1bd45c797446ae83ae72f8a2360b25a5c818
Simorico <simorico.dev@gmail.com>
2025-04-05 14:10:21 +1000

    added TripodMounter + Dismounter w/ logic

---
+++ Scripts/Camera/TripodMounter.cs
@@ -0,0 +1,22 @@
+using Godot;
+using MyFathersHomeProject.Scripts.Shared.Helpers;
+
+namespace MyFathersHomeProject.Scripts.Camera;
+public partial class TripodMounter : Area2D
+{
+    private PlayerCamera PlayerCamera => GetNodeHelper.GetPlayerCamera(GetTree());
+    private Marker2D MountPosition => GetNode<Marker2D>("MountPosition");
+    
+    public override void _Ready()
+    {
+        BodyShapeEntered += OnBodyShapeEntered;
+    }
+    
+    private void OnBodyShapeEntered(Rid bodyrid, Node2D body, long bodyshapeindex, long localshapeindex)
+    {
+        if (body is Oliver)
+        {
+            PlayerCamera.Mount(MountPosition.Position);
+        }
+    }
+}
