b4c16f0cc4f8640eb59949f7c5dbadd278b124b6
Simorico <simorico.dev@gmail.com>
2025-04-08 21:09:13 +1000

    added DialogueInteractable
    currently broken, will work once #15 is done

---
+++ Scripts/Shared/Modules/Interactables/DialogueInteractable.cs
@@ -0,0 +1,22 @@
+using Godot;
+using MyFathersHomeProject.Scripts.Shared.Helpers;
+
+namespace MyFathersHomeProject.Scripts.Shared.Modules.Interactables;
+public partial class DialogueInteractable : ItemInteractable
+{
+    [Export] public Resource DialogueScript;
+    [Export] public string DialogueStartString;
+    
+    private Oliver oliver = new();
+    
+    public override void _Ready()
+    { 
+        var tree = GetTree();
+        oliver = GetNodeHelper.GetOliver(tree);
+    }
+
+    public void Interact()
+    {
+        EmitSignal(nameof(CustomSignals.Triggered));
+    }
+}
