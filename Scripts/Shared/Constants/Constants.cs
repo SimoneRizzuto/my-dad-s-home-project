8301b9361be85e5bd18111aadeb7d2e0a7a23498
Simorico <simorico.dev@gmail.com>
2025-04-08 23:52:29 +1000

    added ZIndexLayers constants

--- Scripts/Shared/Constants/Constants.cs
+++ Scripts/Shared/Constants/Constants.cs
@@ -13,3 +13,11 @@ public static class NodeGroup
     public const string Oliver = "oliver";
     public const string PlayerCamera = "player camera";
 }
+
+public static class ZIndexLayers
+{
+    public const int BackgroundLayer = 10;
+    public const int ForegroundLayer = 1000;
+    public const int MinLayer = -4096;
+    public const int MaxLayer = 4096;
+}
