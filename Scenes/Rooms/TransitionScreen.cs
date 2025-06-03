b9914286fe8f6d33b806e36c4aa44eb992f1ccde
Dominic Cotter <dominiccotter17@outlook.com>
2025-06-03 21:36:53 +1000

    Create TransitionScreen & Add script

---
+++ Scenes/Rooms/TransitionScreen.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+public partial class TransitionScreen : Control
+{
+    [Export] public PackedScene NextScene;
+    [Export] string richText;
+    
+    private RichTextLabel richTextLabel = new RichTextLabel();
+    private Tween tween;
+
+    public override void _Ready()
+    {
+        richTextLabel = GetNode<RichTextLabel>("RichTextLabel");
+        richTextLabel.Text = richText;
+    }
+
+    private void TypeWriterText()
+    {
+        tween.TweenProperty(richTextLabel, "visible_ratio", 1.0, 2.0).From(0.0);
+    }
+}
