70701e100c8776d764cab9f6008523aaa7ebcbd9
Dominic Cotter <dominiccotter17@outlook.com>
2025-07-08 21:32:19 +1000

    Ammend centering of text. Add in fade in and fade out of text.

--- Scenes/Rooms/TransitionScreen.cs
+++ Scenes/Rooms/TransitionScreen.cs
@@ -15,18 +15,17 @@ public partial class TransitionScreen : Control
     public override void _Ready()
     {
         _sceneSwitcher = SceneSwitcher.Instance;
-        AddChild(_richTextLabel);
-        _richTextLabel = GetNode<Label>("RichTextLabel");
+        _richTextLabel = GetNode<Label>("Label");
         _richTextLabel.Text = RichText;
-        
-        _stopwatch.Start();
+        CenterLabelOnScreen(_richTextLabel);
+        FadeInLabel();
     }
     
     public override void _Process(double delta)
     {
         if (!_stopwatch.IsRunning || !(_stopwatch.Elapsed.TotalSeconds >= 2)) return;
         _stopwatch.Stop();
-        CallDeferred(nameof(DoSceneSwitch));
+        FadeOutLabel();
     }
     
     
@@ -35,8 +34,43 @@ public partial class TransitionScreen : Control
         _sceneSwitcher.TransitionToScene(null, NextScene);
     }
 
-    private void TypeWriterText()
+    private void FadeOutLabel()
     {
-        _tween.TweenProperty(_richTextLabel, "visible_ratio", 1.0, 2.0).From(0.0);
+        _tween = GetTree().CreateTween();
+        _tween.TweenProperty(_richTextLabel, "modulate:a", 0.0f, 3.0f);
+        _tween.TweenCallback(Callable.From(DoSceneSwitch));
+    }
+    
+    private void FadeInLabel()
+    {
+        // Ensure label starts fully transparent
+        var color = _richTextLabel.Modulate;
+        color.A = 0.0f;
+        _richTextLabel.Modulate = color;
+
+        _tween = GetTree().CreateTween();
+        _tween.TweenProperty(_richTextLabel, "modulate:a", 1.0f, 3.0f);
+        _tween.TweenCallback(Callable.From(OnFadeInComplete));
+    }
+
+    private void OnFadeInComplete()
+    {
+        _stopwatch.Start();
+    }
+    
+    private void CenterLabelOnScreen(Label label)
+    {
+        // This is done in the engine but for some reason not the same results
+        label.AnchorLeft = 0.5f;
+        label.AnchorTop = 0.5f;
+        label.AnchorRight = 0.5f;
+        label.AnchorBottom = 0.5f;
+        label.OffsetLeft = -label.Size.X / 2;
+        label.OffsetTop = -label.Size.Y / 2;
+        label.OffsetRight = label.Size.X / 2;
+        label.OffsetBottom = label.Size.Y / 2;
+        label.HorizontalAlignment = HorizontalAlignment.Center;
+        label.VerticalAlignment = VerticalAlignment.Center;
+
     }
 }
