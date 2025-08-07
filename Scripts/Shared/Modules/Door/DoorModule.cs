317af662376479f44b8c2b077121fda17d582fcf
Simone Rizzuto <simorico.dev@gmail.com>
2025-08-07 15:28:13 +1000

    door sprite + DoorModule

---
+++ Scripts/Shared/Modules/Door/DoorModule.cs
@@ -0,0 +1,39 @@
+using System;
+using Godot;
+
+namespace MyFathersHomeProject.Scripts.Shared.Modules.Door;
+public partial class DoorModule : Node
+{
+	[Export] public DoorType Type = DoorType.Orange;
+	[Export] public bool Closed;
+	
+	// getters
+	private AnimatedSprite2D DoorSprite => GetNode<AnimatedSprite2D>("/DoorSprite");
+	private string DoorOpenStateString => Closed ? "closed" : "open";
+	private string DoorSpriteToPlay => $"{GetDoorSpriteString(Type)} {DoorOpenStateString}";
+	
+	public override void _EnterTree()
+	{
+		DoorSprite.Play(DoorSpriteToPlay);
+	}
+	
+	private string GetDoorSpriteString(DoorType type)
+	{
+		return type switch
+		{
+			DoorType.Orange => "orange",
+			DoorType.Blue => "blue",
+			DoorType.OliverBlue => "oliver blue",
+			DoorType.Wooden => "wooden",
+			_ => "orange"
+		};
+	}
+}
+
+public enum DoorType
+{
+	Orange,
+	Blue,
+	OliverBlue,
+	Wooden,
+}
