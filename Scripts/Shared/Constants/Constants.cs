8cc992dff441d2491087a803c280c01c70cf2d8d
Simorico <simorico.dev@gmail.com>
2025-08-13 20:11:07 +1000

    made directions have less options

--- Scripts/Shared/Constants/Constants.cs
+++ Scripts/Shared/Constants/Constants.cs
@@ -1,11 +1,8 @@
 namespace MyFathersHomeProject.Scripts.Shared.Constants;
 public enum Direction
 {
-    Up = 0,
     Left = 1,
     Right = 2,
-    Down = 3,
-    None = 4,
 }
 
 public static class NodeGroup
