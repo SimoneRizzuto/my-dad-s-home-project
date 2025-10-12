0599be04191db43bc37d6cb1895701cce6629045
Simorico <simorico.dev@gmail.com>
2025-10-12 20:20:33 +1000

    added basic MenuItemButton logic

--- Scripts/Menus/MenuItemButton.cs
+++ Scripts/Menus/MenuItemButton.cs
@@ -1,6 +1,30 @@
 using Godot;
 using System;
 
+namespace MyFathersHomeProject.Scripts.Menus;
 public partial class MenuItemButton : Button
 {
+	[Export] public ButtonAction Action;
+	[Export] public string SwitchSceneUid = "";
+	
+	public override void _Pressed()
+	{
+		switch (Action)
+		{
+			case ButtonAction.SwitchScene:
+				break;
+			case ButtonAction.ExitGame:
+				
+				// play fade
+				
+				GetTree().Quit();
+				break;
+		}
+	}
+}
+
+public enum ButtonAction
+{
+	SwitchScene,
+	ExitGame,
 }
