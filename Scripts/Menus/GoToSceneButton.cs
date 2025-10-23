2fac3787c64cdbd3c62ff628cce65d2b85e8f819
Simorico <simorico.dev@gmail.com>
2025-10-23 21:40:54 +1000

    added debug buttons

---
+++ Scripts/Menus/GoToSceneButton.cs
@@ -0,0 +1,47 @@
+using Godot;
+using MyFathersHomeProject.Scripts.Singletons.SceneSwitcher;
+
+namespace MyFathersHomeProject.Scripts.Menus;
+public partial class GoToSceneButton : Button
+{
+	[Export] public DebugSceneOptions SceneOptions;
+	
+	public override void _Pressed()
+	{
+		switch (SceneOptions)
+		{
+			case DebugSceneOptions.Set1:
+				SceneSwitcher.Instance?.TransitionToScene(SceneSwitcher.Set1_LivingRoom);
+				break;
+			case DebugSceneOptions.Set2:
+				SceneSwitcher.Instance?.TransitionToScene(SceneSwitcher.Set2_SashaCampfire);
+				break;
+			case DebugSceneOptions.Set3:
+				SceneSwitcher.Instance?.TransitionToScene("");
+				break;
+			case DebugSceneOptions.Set4:
+				SceneSwitcher.Instance?.TransitionToScene("");
+				break;
+			case DebugSceneOptions.Set5:
+				SceneSwitcher.Instance?.TransitionToScene("");
+				break;
+			case DebugSceneOptions.Set6:
+				SceneSwitcher.Instance?.TransitionToScene("");
+				break;
+			case DebugSceneOptions.Set7:
+				SceneSwitcher.Instance?.TransitionToScene("");
+				break;
+		}
+	}
+}
+
+public enum DebugSceneOptions
+{
+	Set1,
+	Set2,
+	Set3,
+	Set4,
+	Set5,
+	Set6,
+	Set7,
+}
