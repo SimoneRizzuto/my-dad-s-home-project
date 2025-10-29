1d46ba6b2550648c739fe5b77629c9116abfa989
Dominic Cotter <dominiccotter17@outlook.com>
2025-10-29 20:23:41 +1000

    Create PauseMenuModule inspired by MainMenu

---
+++ Scripts/Menus/PauseMenuModule.cs
@@ -0,0 +1,92 @@
+using Godot;
+using MyFathersHomeProject.Scripts.Camera;
+using MyFathersHomeProject.Scripts.Shared.Extensions;
+using MyFathersHomeProject.Scripts.Shared.Constants;
+
+namespace MyFathersHomeProject.Scripts.Menus;
+
+public partial class PauseMenuModule : CanvasLayer
+{
+	// pause menu
+	private Control PauseMenu => GetNode<Control>("PauseMenu");
+	private Button LetsContinueButton => GetNode<Button>("%LetsContinueButton");
+	private Button OptionsButton => GetNode<Button>("%OptionsButton");
+	private Button DebugButton => GetNode<Button>("%DebugButton");
+	private Button QuitButton => GetNode<Button>("%QuitButton");
+	
+	// settings menu
+	private Control OptionsMenu => GetNode<Control>("OptionsMenu");
+	private Button TestButton => GetNode<Button>("%TestButton");
+
+	// debug menu
+	private Control DebugMenu => GetNode<Control>("DebugMenu");
+	private Button Set1Button => GetNode<Button>("%Set1Button");
+	
+	// variables
+	private int pauseMenuButtonLastFocusIndex = 0;
+	const double menuFadeDefaultTime = 0.25d;
+	
+	public override void _Ready()
+	{
+		PauseMenu.Visible = false;
+		DebugMenu.Visible = false;
+	}
+	
+	public void GoToPauseMenu()
+	{
+		DebugMenu.FadeOut(menuFadeDefaultTime, () => DebugMenu.Visible = false);
+		
+		PauseMenu.Visible = true;
+		PauseMenuFocus();
+		PauseMenu.FadeIn(menuFadeDefaultTime);
+	}
+	
+	public void GoToMainMenu()
+	{
+		OptionsMenu.FadeOut(0.25d, () => OptionsMenu.Visible = false);
+		DebugMenu.FadeOut(0.25d, () => DebugMenu.Visible = false);
+		
+		// Need to scene transition here to MainMenu
+		/*MainMenu.Visible = true;
+		MainMenuFocus();
+		MainMenu.FadeIn(0.25d);*/
+	}
+	
+	public void GoToSettingsMenu()
+	{
+		PauseMenu.FadeOut(menuFadeDefaultTime, () => PauseMenu.Visible = false);
+		DebugMenu.FadeOut(menuFadeDefaultTime, () => DebugMenu.Visible = false);
+		
+		OptionsMenu.Visible = true;
+		TestButton.GrabFocus();
+		OptionsMenu.FadeIn(menuFadeDefaultTime);
+		
+		pauseMenuButtonLastFocusIndex = 1;
+	}
+	public void GoToDebugMenu()
+	{
+		PauseMenu.FadeOut(menuFadeDefaultTime, () => PauseMenu.Visible = false);
+		
+		DebugMenu.Visible = true;
+		Set1Button.GrabFocus();
+		DebugMenu.FadeIn(menuFadeDefaultTime);
+		
+		pauseMenuButtonLastFocusIndex = 2;
+	}
+	
+	private void PauseMenuFocus()
+	{
+		switch (pauseMenuButtonLastFocusIndex)
+		{
+			case 0:
+				LetsContinueButton.GrabFocus();
+				break;
+			case 1:
+				OptionsButton.GrabFocus();
+				break;
+			case 2:
+				DebugButton.GrabFocus();
+				break;
+		}
+	}
+}
