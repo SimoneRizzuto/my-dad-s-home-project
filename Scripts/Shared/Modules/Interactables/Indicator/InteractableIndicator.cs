6573e1a0753c5aa80a65de361d73592e27d27776
Simone Rizzuto <simorico.dev@gmail.com>
2025-10-30 18:01:30 +1000

    centered the bubble on the interactable

--- Scripts/Shared/Modules/Interactables/Indicator/InteractableIndicator.cs
+++ Scripts/Shared/Modules/Interactables/Indicator/InteractableIndicator.cs
@@ -6,21 +6,33 @@ using MyFathersHomeProject.Scripts.Singletons.InteractionAllocator;
 namespace MyFathersHomeProject.Scripts.Shared.Modules.Interactables.Indicator;
 public partial class InteractableIndicator : AnimatedSprite2D
 {
+	// consts
 	private const int HeightAdjustment = 25;
 	
 	public override void _Process(double delta)
 	{
-		if (InteractionAllocator.Instance == null || Oliver.Instance == null) return;
-		
 		ProcessVisibility();
+		ProcessGlobalPosition();
+	}
+
+	private void ProcessGlobalPosition()
+	{
+		if (InteractionAllocator.Instance == null) return;
+		
+		var texture = SpriteFrames.GetFrameTexture(Animation, Frame);
+		var width = texture.GetSize().X;
+		var center = width / 2;
 		
 		var position = InteractionAllocator.Instance.ClosestInteractable.GlobalPosition;
-		Position = new(position.X, position.Y - HeightAdjustment);
+		var x = position.X - center;
+		var y = position.Y - HeightAdjustment;
+		GlobalPosition = new(x, y);
 	}
-	
+
 	private void ProcessVisibility()
 	{
 		if (InteractionAllocator.Instance == null || Oliver.Instance == null) return;
+		
 		var isColliding = InteractionAllocator.Instance.ClosestInteractable.OliverIsColliding;
 		var isCutscene = Oliver.Instance.CharacterState != CharacterState.Cutscene;
 		Visible =  isColliding && isCutscene;
