933550972da4c048cb4a0e96f208d631b9e8a421
Simone Rizzuto <simorico.dev@gmail.com>
2025-11-01 10:55:33 +1000

    microadjustments to logic

--- Scripts/Shared/Modules/Interactables/Custom/ClothesMoveToHeadAction.cs
+++ Scripts/Shared/Modules/Interactables/Custom/ClothesMoveToHeadAction.cs
@@ -9,6 +9,7 @@ public partial class ClothesMoveToHeadAction : Node, IAction
 	[Export] public Texture2D? PickUpTexture;
 	
 	// getters
+	private Node CurrentScene => GetTree().CurrentScene;
 	private Sprite2D? ClothesSprite2D => GetParent()?.GetParent<Sprite2D>();
 	private Oliver? Oliver => Oliver.Instance;
 	
@@ -18,6 +19,10 @@ public partial class ClothesMoveToHeadAction : Node, IAction
 		
 		if (ClothesSprite2D != null && PickUpTexture != null)
 		{
+			var bedsideDrawer = GetParent().GetParent().GetParent()
+                .GetNode("%BedsideDrawer")
+                .GetNode<InteractableModule>("InteractableModule");
+			
 			ClothesSprite2D.GetParent().RemoveChild(ClothesSprite2D);
 			Oliver.AddChild(ClothesSprite2D);
 			
@@ -25,10 +30,13 @@ public partial class ClothesMoveToHeadAction : Node, IAction
 			ClothesSprite2D.Position = new(-5, -22);
 			
 			SceneStates.Instance.ClothesPickedUp = true;
+			
+			if (bedsideDrawer != null)
+			{
+				bedsideDrawer.ProcessMode = ProcessModeEnum.Inherit;
+			}
 		}
 		
-		// activate other Action to place into drawer
-		// maybe through signal??
 		QueueFree();
 	}
 }
