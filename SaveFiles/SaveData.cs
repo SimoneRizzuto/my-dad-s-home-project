14101cd7b1106a954acb9801b76d07f33d230cb8
Dominic Cotter <dominiccotter17@outlook.com>
2025-12-22 18:31:24 +1000

    Rename & add files
    make suggested changes from Sim

---
+++ SaveFiles/SaveData.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+
+namespace MyFathersHomeProject.SaveFiles;
+
+public class SaveData
+{
+	public int SceneIndex { get; set; } = 0;
+	public string SceneUID { get; set; } = "";
+	public Godot.Collections.Array<string> SceneNames { get; set; } = new();
+}
