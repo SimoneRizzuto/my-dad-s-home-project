08d7edfd7b988a790d0453c6250251519f9fe736
Simorico <simorico.dev@gmail.com>
2026-02-24 21:12:04 +1000

    added Fade.gd as FadeAutoload autoload

--- Scripts/Shared/Modules/Fade/FadeWrapper.gd
+++ Scripts/Shared/Modules/Fade/FadeWrapper.gd
@@ -3,16 +3,16 @@ extends Node2D
 signal finished
 
 func fade_out(time := 1.0, color := Color.BLACK, pattern := "", reverse := false, smooth := false):
-	if Fade:
-		await Fade.fade_out(time, color, pattern, reverse, smooth).finished
+	if FadeAutoload:
+		await FadeAutoload.fade_out(time, color, pattern, reverse, smooth).finished
 		finished.emit()
 	else:
 		print("\"Fade\" was not available to fade out.")
 
 ## Fades in the screen, so it's visible again. Use the parameters to customize it.
 func fade_in(time := 1.0, color := Color.BLACK, pattern := "", reverse := true, smooth := false):
-	if Fade:
-		await Fade.fade_in(time, color, pattern, reverse, smooth).finished
+	if FadeAutoload:
+		await FadeAutoload.fade_in(time, color, pattern, reverse, smooth).finished
 		finished.emit()
 	else:
 		print("\"Fade\" was not available to fade in.")
