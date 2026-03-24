cb6dcf9a16ff1a7226b59c364bce19d9d0341a9e
Dominic Cotter <dominiccotter17@outlook.com>
2026-03-24 19:23:57 +1000

    Updated & tested new save logic
    test save to appdata for project
    remove save test
    transition scene set up to save when initialised

--- SaveFiles/SaveData.cs
+++ SaveFiles/SaveData.cs
@@ -1,19 +1,9 @@
-using System.Collections.Generic;
-
 namespace MyFathersHomeProject.SaveFiles;
-
 public class SaveData
 {
-	public int TestIndex { get; set; } = 0;
-	public string TestUid { get; set; } = "";
-	public List<string> TestNames { get; set; } = new();
-
-	public class MyClass
+	public SaveData(int setId)
 	{
-		public int TestInt2 { get; set; } = 100;
-		private int TestInt3 { get; set; } = 200;
-		public List<string> TestNames2 { get; set; } = new();
+		SetId = setId;
 	}
-
-	public MyClass TestMtClass { get; set; } = new();
+	public int SetId { get; set; } = 0;
 }
