04e8b52b068c56db903af1ec73de39b2ec3e279e
Dominic Cotter <dominiccotter17@outlook.com>
2026-04-20 21:17:09 +1000

    Cleanup

--- Scenes/Dialogue/Credits.cs
+++ Scenes/Dialogue/Credits.cs
@@ -1,7 +1,8 @@
 using Godot;
 using MyFathersHomeProject.Scripts.Camera;
-using MyFathersHomeProject.Scripts.Shared.Extensions;
 using MyFathersHomeProject.Scripts.Singletons.SceneSwitcher;
+
+namespace MyFathersHomeProject.Scenes.Dialogue;
 
 public partial class Credits : Control
 {
@@ -21,23 +22,19 @@ public partial class Credits : Control
 
 	public override void _Process(double delta)
 	{
-		// Quit or change scene when done
 		if (CreditsContainer.Position.Y + CreditsContainer.Size.Y < 100)
 		{
 			WaitForEffect();
 			Scrolling = true;
 			return;
 		}
-		
-		// Move credits upward
+
 		CreditsContainer.Position -= new Vector2(0, ScrollSpeed * (float)delta);
-		
 	}
 
 	private void WaitForEffect()
 	{
 		if (Scrolling) return;
-		// Start Stopwatch
 		ScrollTimer.Start(5.0f);
 	}
 
@@ -45,15 +42,14 @@ public partial class Credits : Control
 	{
 		RemoveChild(ScrollTimer);
 		ScrollTimer.QueueFree();
-		
-		// Fade Credits & Return to Main Menu
+
 		if (FadeUtil.Instance != null)
 		{
 			FadeUtil.Instance.FadeOut(5);
 			FadeUtil.Instance.FadeFinished += ReturnToMainMenu;
 		}
-		
 	}
+
 	private void ReturnToMainMenu()
 	{
 		SceneSwitcher.Instance?.TransitionToScene(SceneSwitcher.MainMenuTrigger, false);
@@ -62,14 +58,4 @@ public partial class Credits : Control
 			FadeUtil.Instance.FadeFinished -= ReturnToMainMenu;
 		}
 	}
-	
-
-	/*public override void _Input(InputEvent @event)
-	{
-		if (@event.IsActionPressed("ui_accept"))
-		{
-			// Skip credits
-			GetTree().Quit();
-		}
-	}*/
 }
