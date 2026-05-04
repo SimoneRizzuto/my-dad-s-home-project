532ef11f44fdf52571de53e706c2e94d7bde8d95
Simone Rizzuto <simorico.dev@gmail.com>
2026-05-04 16:02:35 +1000

    added jump

--- Scripts/Dialogue/Actor/ActorModule.cs
+++ Scripts/Dialogue/Actor/ActorModule.cs
@@ -7,6 +7,7 @@ using MyFathersHomeProject.Scripts.Shared.Constants;
 namespace MyFathersHomeProject.Scripts.Dialogue.Actor;
 public partial class ActorModule : Node2D, IAsyncDialogueVariables
 {
+    // getters
     public CharacterBody2D CharacterBody => GetParentOrNull<CharacterBody2D>();
     public AnimatedSprite2D MainSprite => GetParent().GetNode<AnimatedSprite2D>($"{nameof(MainSprite)}");
     public CollisionShape2D MainShape => GetParent().GetNode<CollisionShape2D>($"{nameof(MainShape)}");
@@ -23,6 +24,7 @@ public partial class ActorModule : Node2D, IAsyncDialogueVariables
     private int unitsToMove;
     private int targetXToMoveTo;
     private int initialXPosition;
+    private bool _isLoopingJump;
 
     public async Task Idle()
     {
@@ -102,6 +104,16 @@ public partial class ActorModule : Node2D, IAsyncDialogueVariables
         Character.SetHoldingPlate(isHolding);
     }
     
+    public void StartJumpLoop()
+    {
+        _isLoopingJump = true;
+    }
+
+    public void StopJumpLoop()
+    {
+        _isLoopingJump = false;
+    }
+    
     #endregion
     
     #region process logic
@@ -124,6 +136,11 @@ public partial class ActorModule : Node2D, IAsyncDialogueVariables
     public override void _PhysicsProcess(double delta)
     {
         if (Character.CharacterState != CharacterState.Cutscene) return;
+        
+        if (_isLoopingJump && !Character.IsJumping)
+        {
+            Character.Jump();
+        }
         
         switch (dialogueDirectionToPlay)
         {
