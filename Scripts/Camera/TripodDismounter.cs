736a6a460c5f22f3c97c479086f5275b2bc45eb2
Simone Rizzuto <simorico.dev@gmail.com>
2026-05-04 16:41:28 +1000

    more progress

--- Scripts/Camera/TripodDismounter.cs
+++ Scripts/Camera/TripodDismounter.cs
@@ -1,20 +1,46 @@
 using Godot;
+using System;
 using MyFathersHomeProject.Scripts.Player;
 
 namespace MyFathersHomeProject.Scripts.Camera;
-public partial class TripodDismounter : Area2D
+public partial class TripodDismounter : Area2D, IDisposable
 {
+	[Export] public float DismountDelay = 0.2f;
+	[Export] public bool SmoothDismount;
+
+	private SceneTreeTimer? _dismountTimer;
+
 	public override void _Ready()
 	{
 		BodyShapeEntered += OnBodyShapeEntered;
 	}
-	
+
 	private void OnBodyShapeEntered(Rid bodyrid, Node2D body, long bodyshapeindex, long localshapeindex)
 	{
 		if (body is Oliver)
 		{
-			PlayerCamera.Instance.ToggleSmoothing(true);
+			PlayerCamera.Instance.ToggleSmoothing(SmoothDismount);
 			PlayerCamera.Instance.Dismount();
+
+			_dismountTimer = GetTree().CreateTimer(DismountDelay);
+			_dismountTimer.Timeout += OnDismountDelayFinished;
+		}
+	}
+
+	private void OnDismountDelayFinished()
+	{
+		PlayerCamera.Instance.ToggleSmoothing(true);
+		_dismountTimer = null;
+	}
+
+	public void Dispose()
+	{
+		BodyShapeEntered -= OnBodyShapeEntered;
+
+		if (_dismountTimer != null)
+		{
+			_dismountTimer.Timeout -= OnDismountDelayFinished;
+			_dismountTimer = null;
 		}
 	}
 }
